9d3f37dd89e3c1ebf4407be75879646fb3a6fc9a
José Fonseca <jose.fonseca@mindera.com>
2018-02-09 22:30:11 +0000

    feat(dev-model): Introduce development model scoring system from 1-5 following the backend scoring strategy

--- src/modules/autismRocksModel.js
+++ src/modules/autismRocksModel.js
@@ -1,259 +1,259 @@
 import i18n from 'locales/i18n'
 import Participant from 'models/participant'
 import {
-  JetView
+    JetView
 } from 'webix-jet'
 
 
 function scaleCellTemplate(obj, level) {
-  if (obj.scale) {
-    return '<span class="scale-header-option">' + i18n.t('scale.' + obj.scale + '.' + level) + '</span>';
-  } else if (obj.question) {
-    let checked = '';
-    if (obj.current_level == level || (obj.previous_level == level && !obj.current_level)) {
-      checked = 'checked'
+    if (obj.scale) {
+        return '<span class="scale-header-option">' + i18n.t('scale.' + obj.scale + '.' + level) + '</span>';
+    } else if (obj.question) {
+        let checked = '';
+        if (obj.current_level == level || (obj.previous_level == level && obj.current_level === null)) {
+            checked = 'checked'
+        }
+        return '<input type="radio" name="modelRowOption' + obj._id + '" value="' + level + '" ' + checked + '/>'
+    } else {
+        return '';
     }
-    return '<input type="radio" name="modelRowOption' + obj._id + '" value="' + level + '" ' + checked + '/>'
-  } else {
-    return '';
-  }
 }
 
 const treetable = {
-  view: "treetable",
-  id: "developmentModelTreeTable",
-  css: 'devmodel-form',
-  editable: true,
-  hover: "hover",
-  scheme: {
+    view: "treetable",
+    id: "developmentModelTreeTable",
+    css: 'devmodel-form',
+    editable: true,
+    hover: "hover",
+    scheme: {
 
-    $change: function(obj) {
-      if (obj.question && !obj.current_level) {
-        obj.$css = 'devmodel-required';
-      } else {
-        obj.$css = '';
-      }
+        $change: function (obj) {
+            if (obj.question && obj.current_level === null) {
+                obj.$css = 'devmodel-required';
+            } else {
+                obj.$css = '';
+            }
 
-      if (obj.scale) {
-        obj.$css += ' scale-header';
-        obj.$height = 50;
-      }
+            if (obj.scale) {
+                obj.$css += ' scale-header';
+                obj.$height = 50;
+            }
 
-      if (obj.id.split('.').length == 1) {
-        obj.$css += ' top-level-header';
-      }
+            if (obj.id.split('.').length == 1) {
+                obj.$css += ' top-level-header';
+            }
 
-      // if (obj.question && obj.question.length > 100) {
-      //   obj.$height = 55;
-      // }
-    }
-  },
-  tooltip: {
-    template: function(obj) {
-      return obj.description ? '<div class="tooltip-description">'+obj.description+'</div>' : '';
-    }
-  },
-  columns: [{
-      id: 'ref',
-      header: 'Ref',
-      width: 60
-    },
-    {
-      id: "group",
-      header: i18n.t('development_model.form.question'),
-      fillspace: true,
-      template: function(obj, common) {
-        let text = '';
-
-        if (obj.description) {
-          text += '<span class="webix_icon fa-question-circle"></span>';
+            // if (obj.question && obj.question.length > 100) {
+            //   obj.$height = 55;
+            // }
         }
+    },
+    tooltip: {
+        template: function (obj) {
+            return obj.description ? '<div class="tooltip-description">' + obj.description + '</div>' : '';
+        }
+    },
+    columns: [{
+        id: 'ref',
+        header: 'Ref',
+        width: 60
+    },
+        {
+            id: "group",
+            header: i18n.t('development_model.form.question'),
+            fillspace: true,
+            template: function (obj, common) {
+                let text = '';
 
-        text += "<span class='question-title'>" + (obj.group ? obj.group : obj.question) + "</span>";
+                text += "<span class='question-title'>" + (obj.group ? obj.group : obj.question) + "</span>";
 
-        return common.treetable(obj, common) + text;
-      }
-    },
-    {
-      id: "level1",
-      header: '',
-      width: 90,
-      template: function(obj) {
-        return scaleCellTemplate(obj, 1)
-      }
-    },
-    {
-      id: "level2",
-      header: '',
-      width: 90,
-      template: function(obj) {
-        return scaleCellTemplate(obj, 2)
-      }
-    },
-    {
-      id: "level3",
-      header: '',
-      width: 90,
-      template: function(obj) {
-        return scaleCellTemplate(obj, 3)
-      }
-    },
-    {
-      id: "level4",
-      header: '',
-      width: 90,
-      template: function(obj) {
-        return scaleCellTemplate(obj, 4)
-      }
-    },
-    {
-      id: "level5",
-      header: '',
-      width: 90,
-      template: function(obj) {
-        return scaleCellTemplate(obj, 5)
-      }
-    }
-  ],
-  on: {
-
-    onBeforeEditStart: function(id) {
-      if (id.column.indexOf('level') == 0) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-
-    onEditorChange: function(id, value) {
-      var item = this.getItem(id.row);
-      $$("developmentModelTreeTable").editStop();
-    },
-
-    onItemClick: function(id) {
-      var item = this.getItem(id.row);
-
-      if (id.column.indexOf('level') == 0) {
-        item.current_level = id.column.replace('level', '');
-        item.$css = '';
-        let table = $$("developmentModelTreeTable");
-        webix.ajax()
-          .headers({
-            'Content-type': 'application/json'
-          })
-          .post(table.config.url, JSON.stringify(item))
-          .then((res) => {
-            var enableSaveButton = true;
-            table.eachRow(
-              function(row) {
-                let item = table.getItem(row);
-                if (item.question) {
-                  enableSaveButton = enableSaveButton && item.current_level;
+                if (obj.description) {
+                    text += '<span class="webix_icon fa-question-circle"></span>';
                 }
-              }
-            );
 
-            if (enableSaveButton) {
-              $$('saveDevelopmentModel').define('disabled', false);
+                return common.treetable(obj, common) + text;
             }
+        },
+        {
+            id: "level0",
+            header: '',
+            width: 90,
+            template: function (obj) {
+                return scaleCellTemplate(obj, 0)
+            }
+        },
+        {
+            id: "level1",
+            header: '',
+            width: 90,
+            template: function (obj) {
+                return scaleCellTemplate(obj, 1)
+            }
+        },
+        {
+            id: "level3",
+            header: '',
+            width: 90,
+            template: function (obj) {
+                return scaleCellTemplate(obj, 3)
+            }
+        },
+        {
+            id: "level6",
+            header: '',
+            width: 90,
+            template: function (obj) {
+                return scaleCellTemplate(obj, 6)
+            }
+        },
+        {
+            id: "level10",
+            header: '',
+            width: 90,
+            template: function (obj) {
+                return scaleCellTemplate(obj, 10)
+            }
+        }
+    ],
+    on: {
 
-          })
-          .fail((res) => {
-            window.console.error(res);
-            let errorMessage = i18n.t('ERROR_UPDATING_EVALUATION');
-            if (res.responseText) {
-              errorMessage = i18n.t(JSON.parse(res.responseText).message);
+        onBeforeEditStart: function (id) {
+            if (id.column.indexOf('level') == 0) {
+                return true;
+            } else {
+                return false;
             }
-            webix.message({
-              text: errorMessage,
-              type: 'error',
-              expire: 4000
-            });
-          });
-      }
+        },
+
+        onEditorChange: function (id, value) {
+            var item = this.getItem(id.row);
+            $$("developmentModelTreeTable").editStop();
+        },
+
+        onItemClick: function (id) {
+            var item = this.getItem(id.row);
+
+            if (id.column.indexOf('level') == 0) {
+                item.current_level = id.column.replace('level', '');
+                item.$css = '';
+                let table = $$("developmentModelTreeTable");
+                webix.ajax()
+                    .headers({
+                        'Content-type': 'application/json'
+                    })
+                    .post(table.config.url, JSON.stringify(item))
+                    .then((res) => {
+                        var enableSaveButton = true;
+                        table.eachRow(
+                            function (row) {
+                                let item = table.getItem(row);
+                                if (item.question) {
+                                    enableSaveButton = enableSaveButton && item.current_level;
+                                }
+                            }
+                        );
+
+                        // if (enableSaveButton) {
+                            $$('saveDevelopmentModel').define('disabled', false);
+                        // }
+
+                    })
+                    .fail((res) => {
+                        window.console.error(res);
+                        let errorMessage = i18n.t('ERROR_UPDATING_EVALUATION');
+                        if (res.responseText) {
+                            errorMessage = i18n.t(JSON.parse(res.responseText).message);
+                        }
+                        webix.message({
+                            text: errorMessage,
+                            type: 'error',
+                            expire: 4000
+                        });
+                    });
+            }
+        }
     }
-  }
 };
 
 const toolbar = {
-  view: "toolbar",
-  cols: [{},
-    {
-      id: "saveDevelopmentModelDate",
-      view: "datepicker",
-      options: [{
-        id: null,
-        value: 'Today'
-      }],
-      width: 150,
-      align: "right"
-    },
-    {
-      view: "button",
-      id: "saveDevelopmentModel",
-      value: i18n.t('development_model.form.save'),
-      align: "right",
-      width: 200,
-      disabled: true,
-      click: function() {
-        webix.ajax()
-          .headers({
-            'Content-type': 'application/json'
-          })
-          .post($$("developmentModelTreeTable").config.url + '/record', {
-            date: $$("saveDevelopmentModelDate").getValue()
-          })
-          .then((res) => res.json())
-          .then((res) => {
-            $$('saveDevelopmentModel').define('disabled', true);
-            webix.message({
-              text: i18n.t(res.message),
-              type: 'success',
-              expire: 4000
-            });
-            $$('developmentModelTreeTable').load($$("developmentModelTreeTable").config.url);
-            // $$('developmentModelTreeTable').refresh();
-          })
-          .fail((res) => {
-            window.console.error(res);
-            let errorMessage = i18n.t('ERROR_RECORDING_PROGRESS');
-            if (res.responseText) {
-              errorMessage = i18n.t(JSON.parse(res.responseText).message);
+    view: "toolbar",
+    cols: [{},
+        {
+            id: "saveDevelopmentModelDate",
+            view: "datepicker",
+            options: [{
+                id: null,
+                value: 'Today'
+            }],
+            width: 150,
+            align: "right"
+        },
+        {
+            view: "button",
+            id: "saveDevelopmentModel",
+            value: i18n.t('development_model.form.save'),
+            align: "right",
+            width: 200,
+            disabled: true,
+            click: function () {
+                webix.ajax()
+                    .headers({
+                        'Content-type': 'application/json'
+                    })
+                    .post($$("developmentModelTreeTable").config.url + '/record', {
+                        date: $$("saveDevelopmentModelDate").getValue()
+                    })
+                    .then((res) => res.json())
+                    .then((res) => {
+                        $$('saveDevelopmentModel').define('disabled', true);
+                        webix.message({
+                            text: i18n.t(res.message),
+                            type: 'success',
+                            expire: 4000
+                        });
+                        $$('developmentModelTreeTable').load($$("developmentModelTreeTable").config.url);
+                        // $$('developmentModelTreeTable').refresh();
+                    })
+                    .fail((res) => {
+                        window.console.error(res);
+                        let errorMessage = i18n.t('ERROR_RECORDING_PROGRESS');
+                        if (res.responseText) {
+                            errorMessage = i18n.t(JSON.parse(res.responseText).message);
+                        }
+                        webix.message({
+                            text: errorMessage,
+                            type: 'error',
+                            expire: 4000
+                        });
+                    });
             }
-            webix.message({
-              text: errorMessage,
-              type: 'error',
-              expire: 4000
-            });
-          });
-      }
-    }
-  ]
+        }
+    ]
 };
 
 const layout = {
-  id: 'developmentModelForm',
-  type: 'clean',
-  rows: [toolbar, treetable]
+    id: 'developmentModelForm',
+    type: 'clean',
+    rows: [toolbar, treetable]
 };
 
 
 export default class AutismRocksModel extends JetView {
-  config() {
-    return layout;
-  }
+    config() {
+        return layout;
+    }
 
-  init(ui, url) {
-    // console.log(url);
-  }
+    init(ui, url) {
+        // console.log(url);
+    }
 
-  urlChange(view, url) {
-    let treetableUrl = '/ar/development_model/pt/' + url[0].params.ref + '/' + url[0].params.id;
-    // treetable.url = treetableUrl;
-    $$('saveDevelopmentModel').define('disabled', true);
-    $$("developmentModelTreeTable").clearAll();
-    $$("developmentModelTreeTable").editCancel();
-    $$('developmentModelTreeTable').define('url', treetableUrl);
-    $$('developmentModelTreeTable').refresh();
-  }
+    urlChange(view, url) {
+        let treetableUrl = '/ar/development_model/pt/' + url[0].params.ref + '/' + url[0].params.id;
+        // treetable.url = treetableUrl;
+        $$('saveDevelopmentModel').define('disabled', true);
+        $$("developmentModelTreeTable").clearAll();
+        $$("developmentModelTreeTable").editCancel();
+        $$('developmentModelTreeTable').define('url', treetableUrl);
+        $$('developmentModelTreeTable').refresh();
+    }
 }
--- src/modules/autismRocksSummary.js
+++ src/modules/autismRocksSummary.js
@@ -1,50 +1,116 @@
 import {
-  JetView
+    JetView
 } from 'webix-jet';
-import Participant from 'models/participant';
-import i18n from 'locales/i18n'
+import i18n from 'locales/i18n';
+import palette from 'google-palette';
+
+
+const colors = palette(['tol', 'qualitative'], 8);
 
 const layout = {
-  rows: [{
-      type: 'header',
-      template: "<span class='webix_icon fa-sign-in'></span>" + i18n.t('sidebar.participant.autismrocks'),
-      height: 45
+    rows: [{
+        type: 'header',
+        template: "<span class='webix_icon fa-sign-in'></span>" + i18n.t('sidebar.participant.autismrocks'),
+        height: 45
     },
-    {
-      view: "chart",
-      type: "radar",
-      // width: 800,
-      height: 300,
-      value: "#average_level#",
-      preset: "area",
-      xAxis: {
-        template: "#group#",
-        lineColor: "#A5A5A5"
-      },
-      yAxis: {
-        start: 0,
-        end: 5,
-        step: 1
-      },
-      origin: 0
-    }
-  ]
+        {
+            view: "chart",
+            type: "radar",
+            height: 300,
+            xAxis: {
+                template: "#group#"
+            },
+            yAxis: {
+                start: 1,
+                end: 5,
+                step: 1
+                // ,
+                // template:function(obj){return 6-obj}
+            },
+            origin: 0
+        }
+    ]
 };
 
 export default class AutismRocksSummaryView extends JetView {
-  config() {
-    return layout;
-  }
+    config() {
+        return layout;
+    }
 
-  urlChange(view, url) {
-    // TODO: locale fix
-    let dataUrl = '/ar/development_model/pt/autism-rocks/' + url[0].params.id + '/summary';
-    webix.ajax(dataUrl).then((res) => res.json())
-      .then((summary) => {
-        view.queryView({
-          view: 'chart'
-        }).parse(summary);
-      });
+    urlChange(view, url) {
+        // TODO: locale fix
+        let dataUrl = '/ar/development_model/pt/autism-rocks/' + url[0].params.id + '/scoring?groupByUser=true';
+        webix.ajax(dataUrl).then((res) => res.json())
+            .then((summary) => {
+                let seriesDataTmp = {};
+                let legend = [];
 
-  }
+                let chart = view.queryView({
+                    view: 'chart'
+                });
+
+                chart.removeAllSeries();
+
+                for (let i = 0; i < summary.length; i++) {
+                    let userId = summary[i].label.id;
+                    let userNmame = summary[i].label.name;
+                    let serie = {
+                        value: `#user_${userId}#`,
+                        tooltip: {
+                            template: `#user_${userId}_name#: #user_${userId}#`
+                        },
+                        line: {
+                            color: `#${colors[i]}`,
+                            width: 2
+                        },
+                        areaColor: `#${colors[i]}`,
+                        item: {
+                            color: "#ffffff",
+                            borderColor: `#${colors[i]}`,
+                            radius: 3,
+                            borderWidth: 2,
+                            type: ['r', 'd', 's'][(i % 3)]
+                        }
+                    };
+
+                    legend.push({
+                        text: userNmame,
+                        color: `#${colors[i]}`
+                    });
+                    chart.addSeries(serie);
+                    for (let j = 0; j < summary[i].data.length; j++) {
+                        let g = summary[i].data[j].group;
+                        let score = summary[i].data[j].score;
+                        if (!seriesDataTmp[g]) {
+                            seriesDataTmp[g] = {}
+                        }
+                        seriesDataTmp[g][`user_${userId}`] = score;
+                        seriesDataTmp[g][`user_${userId}_name`] = userNmame;
+                        seriesDataTmp[g][`user_${userId}_inverted`] = 6 - score;
+                    }
+                }
+
+                let seriesData = Object.keys(seriesDataTmp).map(k => {
+                    let row = {group: k};
+                    Object.keys(seriesDataTmp[k]).forEach((uId) => {
+                        row[uId] = seriesDataTmp[k][uId]
+                    });
+                    return row
+                });
+
+                chart.define('legend', {
+                    layout: "y",
+                    width: 110,
+                    align: "right",
+                    valign: "middle",
+                    marker: {
+                        type: "item"
+                    },
+                    values: legend
+                });
+                chart.parse(seriesData);
+                chart.refresh();
+            });
+
+    }
 }
